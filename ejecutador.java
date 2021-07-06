8724e0ffdea59c75d5bb4979bdd8e74c06a834c9
Jhonny Miñan <53890778+JhonnyMinian92@users.noreply.github.com>
2021-07-06 17:03:37 -0500

    Add files via upload

---
+++ ejecutador.java
@@ -0,0 +1,12 @@
+package inicio;
+
+import view.ventana;
+
+public class ejecutador {
+
+    public static void main(String[] args) {
+        ventana v = new ventana();
+        v.setVisible(true);
+    }
+    
+}
